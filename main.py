c9fe951290c7f309e172f3ed3a9dfc800620733c
Mahi Nandigam <mahidhar.nandigam@gmail.com>
2021-01-02 22:59:36 -0600

    first dump

--- main.py
+++ main.py
@@ -1,11 +1,27 @@
+import json
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+
+import requests
+
+TICKER_API_URL = 'https://api.coinmarketcap.com/v1/ticker/'
+crypto = 'bitcoin'
+
+
+def get_latest_crypto_price(coin):
+    response = requests.get(TICKER_API_URL + coin)
+    response_json = response.json()
+
+    return float(response_json[0]['price_usd'])
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    last_price = -1
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    while True:
+
+        price = get_latest_crypto_price(crypto)
+
+        if price != last_price:
+            print('Bitcoin price: ', price)
+            last_price = price
